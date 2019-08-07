9cd4d870b37f378d5d2b42207ea85c3bd05dd700
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2019-08-07 12:33:48 +0200

    Create manova_analysis.R

---
+++ src/manova_analysis.R
@@ -0,0 +1,27 @@
+# MANOVA analysis
+# By Pieterjan Verhelst
+# Pieterjan.Verhelst@ugent.be
+
+# http://www.sthda.com/english/wiki/manova-test-in-r-multivariate-analysis-of-variance
+
+
+library(mvnormtest)
+
+
+
+# 1. Test assumptions ####
+
+# 2.1. Normality
+# Perform Shapiro-Wilk test for multivariate normality
+mshapiro.test()
+
+# 2.2. Homogeneity of Variances
+
+
+# 2.3. Linearity between all pairs of dependent variables, all pairs of covariates, and all dependent variable-covariate pairs in each cell
+
+
+
+# 2. Conduct MANOVA ####
+
+
