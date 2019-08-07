8e16e4f6e3186677e09aed970f5204ed80b20942
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2019-08-07 17:28:05 +0200

    create plots per sampling method

--- src/data_exploration.R
+++ src/data_exploration.R
@@ -16,8 +16,9 @@ list1 <- colnames(fa_data)
 list1 <- list1[-c(1, 26:37)]
 
 
-# Create scatter plots with smoother
+# 1. Create scatter plots with smoother ####
 
+# 1.1 All
 #ggplot(fa_data, aes(x=Date, y=X14.0)) + 
 #  geom_point() +
 #  geom_smooth(method="auto", se=TRUE, fullrange=FALSE, level=0.95) # geom_abline for regression line
@@ -31,13 +32,25 @@ g$x.Date_y.X14
 
 # Save plot
 pdf("./Figures/Fa_date.pdf")
-g                    # makes the actual plot
+g 
 dev.off()
 
 
+# 1.2 Catch method
+
+fa_data_pg <- filter(fa_data, Fishing_method == "Palinggoot")
+fa_data_su <- filter(fa_data, Fishing_method == "Substraat")
+fa_data_kn <- filter(fa_data, Fishing_method == "Kruisnetten")
+
+g <- ggloop(fa_data_kn, aes_loop(x = Date, y = X14.0:X22.6n.3)) %L+%
+  geom_point() %L+%
+  geom_smooth(method="auto", se=TRUE, fullrange=FALSE, level=0.95) # geom_abline for regression line
 
 
 
-
+# Save plot
+pdf("./Figures/Fa_date_kn.pdf")
+g 
+dev.off()
 
 
