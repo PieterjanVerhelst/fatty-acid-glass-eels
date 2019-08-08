95a3ae04dcb1f3c548d2a0701f35131b6e2a9909
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2019-08-08 14:39:29 +0200

    plot fa for glass eels caugh neer pumping station

--- src/data_exploration.R
+++ src/data_exploration.R
@@ -18,7 +18,7 @@ list1 <- list1[-c(1, 26:37)]
 
 # 1. Create scatter plots with smoother ####
 
-# 1.1 All
+# 1.1 All ==============
 #ggplot(fa_data, aes(x=Date, y=X14.0)) + 
 #  geom_point() +
 #  geom_smooth(method="auto", se=TRUE, fullrange=FALSE, level=0.95) # geom_abline for regression line
@@ -36,7 +36,7 @@ g
 dev.off()
 
 
-# 1.2 Catch method
+# 1.2 Catch method ==============
 
 # Per catch method
 fa_data_pg <- filter(fa_data, Fishing_method == "Palinggoot")
@@ -71,7 +71,7 @@ dev.off()
 
 
 
-# 1.3. Weight and catch method ####
+# 1.3. Weight and catch method ==============
 g <- ggloop(fa_data_no_kn, aes_loop(x = Weight, y = X14.0:X22.6n.3)) %L+%
   geom_point() %L+%
   geom_point(aes(colour = factor(Fishing_method)), size = 2) %L+%
@@ -85,10 +85,53 @@ dev.off()
 
 
 
+# 1.4. Different locations of substrates ==============
+
+unique(fa_data_su$Location)
+
+fa_data_su$Location_type <- NA
+
+for (i in 1:dim(fa_data_su)[1]){
+  if (fa_data_su$Location[i] == "VA_PG_LO_SU" | fa_data_su$Location[i] == "VA_PG_RO_SU"){
+    fa_data_su$Location_type[i] = "PG"
+  } else if (fa_data_su$Location[i] == "VA_MI_RO_SU"){
+    fa_data_su$Location_type[i] = "MI"
+  } else{
+    fa_data_su$Location_type[i] = "SC"
+  }}
+
+fa_data_su$Location_type <- factor(fa_data_su$Location_type)
 
 
+g <- ggloop(fa_data_su, aes_loop(x = Date, y = X14.0:X22.6n.3)) %L+%
+  geom_point() %L+%
+  geom_point(aes(colour = factor(Location_type)), size = 2) %L+%
+  geom_smooth(aes(colour = factor(Location_type)), method="auto", se=TRUE, fullrange=FALSE, level=0.95) # geom_abline for regression line
 
 
+# Save plot
+pdf("./Figures/Fa_date_locationtype.pdf")
+g 
+dev.off()
+
+
+# 1.5 Substrates near pumping station and eel ladders ==============
+
+su_pump <- filter(fa_data_su, Location_type == "PG")
+su_pump$Location_type <- NULL
+su_pg <- rbind(su_pump, fa_data_pg)
+
+
+g <- ggloop(su_pg, aes_loop(x = Date, y = X14.0:X22.6n.3)) %L+%
+  geom_point() %L+%
+  geom_point(aes(colour = factor(Fishing_method)), size = 2) %L+%
+  geom_smooth(aes(colour = factor(Fishing_method)), method="auto", se=TRUE, fullrange=FALSE, level=0.95) # geom_abline for regression line
+
+
+# Save plot
+pdf("./Figures/pumpingstation.pdf")
+g 
+dev.off()
 
 
 
