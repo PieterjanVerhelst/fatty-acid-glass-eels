a83ae090d2f3c0b83991af3b19161d65aec2031b
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2019-08-08 12:04:52 +0200

    plot fa according to fishing method and weight

--- src/data_exploration.R
+++ src/data_exploration.R
@@ -38,6 +38,7 @@ dev.off()
 
 # 1.2 Catch method
 
+# Per catch method
 fa_data_pg <- filter(fa_data, Fishing_method == "Palinggoot")
 fa_data_su <- filter(fa_data, Fishing_method == "Substraat")
 fa_data_kn <- filter(fa_data, Fishing_method == "Kruisnetten")
@@ -46,11 +47,49 @@ g <- ggloop(fa_data_kn, aes_loop(x = Date, y = X14.0:X22.6n.3)) %L+%
   geom_point() %L+%
   geom_smooth(method="auto", se=TRUE, fullrange=FALSE, level=0.95) # geom_abline for regression line
 
-
-
 # Save plot
 pdf("./Figures/Fa_date_kn.pdf")
 g 
 dev.off()
+
+
+
+# Different catch methods in 1 plot
+fa_data_no_kn <- filter(fa_data, Fishing_method == "Palinggoot" |
+                        Fishing_method == "Substraat")
+
+g <- ggloop(fa_data_no_kn, aes_loop(x = Date, y = X14.0:X22.6n.3)) %L+%
+  geom_point() %L+%
+  geom_point(aes(colour = factor(Fishing_method)), size = 2) %L+%
+  geom_smooth(aes(colour = factor(Fishing_method)), method="auto", se=TRUE, fullrange=FALSE, level=0.95) # geom_abline for regression line
+
+
+# Save plot
+pdf("./Figures/Fa_date_Fishingmethods.pdf")
+g 
+dev.off()
+
+
+
+# 1.3. Weight and catch method ####
+g <- ggloop(fa_data_no_kn, aes_loop(x = Weight, y = X14.0:X22.6n.3)) %L+%
+  geom_point() %L+%
+  geom_point(aes(colour = factor(Fishing_method)), size = 2) %L+%
+  geom_smooth(aes(colour = factor(Fishing_method)), method="auto", se=TRUE, fullrange=FALSE, level=0.95) # geom_abline for regression line
+
+
+# Save plot
+pdf("./Figures/Fa_weight_Fishingmethods.pdf")
+g 
+dev.off()
+
+
+
+
+
+
+
+
+
 
 
