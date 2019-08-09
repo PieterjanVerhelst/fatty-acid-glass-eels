42475890bc0ca6503e8a6f0af4c8aabfb0667f0f
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2019-08-09 17:08:48 +0200

    perform permanova

--- README.md
+++ README.md
@@ -11,4 +11,4 @@ Fatty acid analysis on glass eels caught at the Veurne-Ambacht Canal in Nieuwpoo
 1. `preprocessing.R`: Format and process the glass eel metadata
 2. `upload_fa.R`: Upload fatty acid dataset and merge with glass eel metadata
 3. `data_exploration.R`: Apply data exploration
-4. `manova_analysis.R`: Apply multivariate analysis of variance (MANOVA)
+4. `permanova_analysis.R`: Apply Permutational Multivariate Analysis of Variance (PERMANOVA)
---
+++ src/permanova_analysis.R
@@ -0,0 +1,43 @@
+# PERMANOVA analysis
+# By Pieterjan Verhelst
+# Pieterjan.Verhelst@ugent.be
+
+# http://www.sthda.com/english/wiki/manova-test-in-r-multivariate-analysis-of-variance
+
+
+library(mvnormtest)
+
+
+
+# 1. PERMANOVA ####
+
+# Create dataset - do not take into account "kruisnetten"
+subset <- filter(fa_data, Fishing_method == "Palinggoot" |
+                   Fishing_method == "Substraat")
+
+# FA data
+df <- subset[,c(2:25)] 
+# Meta data
+meta <- subset[,c(26:33, 37)]
+
+# Perform PERMANOVA
+# R2 gives the explained variance
+adonis(df~meta$cdate + meta$Fishing_method + meta$Condition_factor, permutations = 999, method = 'bray')
+
+
+# 2. PERMDISP ####
+#An important assumption for PERMANOVA is to have similar "multivariate spread" among groups, which is similar to variance homogeneity in univariate ANOVA. To test if one or more groups is more variable than the others, a PERMDISP can be conducted in R. 
+
+# Create distance matrix
+d_df <- vegdist(df, method="bray") 
+
+bd <- betadisper(d_df,meta$cdate, type = "centroid")
+boxplot(bd)
+permutest(bd)   # Differences between the dispersions exist (p < 0.05)
+
+
+
+
+
+
+
