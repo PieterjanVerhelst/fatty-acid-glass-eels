14d020b6e3d7717dd19d43aa31f46f0294c00f91
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2019-08-10 20:46:58 +0200

    Update permanova_analysis.R

--- src/permanova_analysis.R
+++ src/permanova_analysis.R
@@ -22,14 +22,34 @@ meta <- subset[,c(26:33, 37)]
 
 # Perform PERMANOVA
 # R2 gives the explained variance
-adonis(df~meta$cdate + meta$Fishing_method + meta$Condition_factor, permutations = 999, method = 'bray')
+adonis(df~meta$cdate, permutations = 999, method = 'euclidean')
+
+
+# Marginal tests: the separate effect of each environmental variable on the response matrix
+adonis2(vegdist(df, "euclidean") ~ meta$cdate + meta$Fishing_method + meta$Condition_factor, by = "margin")
+
+# Conditional test: the interaction between environmental variables
+adonis2(vegdist(df, "euclidean") ~ meta$cdate + meta$Fishing_method + meta$Condition_factor, by = "terms")
+
+dbRDA <- capscale(vegdist(df, "euclidean") ~ meta$cdate + meta$Fishing_method + meta$Condition_factor)
+plot(dbRDA)
+summary(dbRDA)
+
+
+
+
+
+
+
+
+
 
 
 # 2. PERMDISP ####
 #An important assumption for PERMANOVA is to have similar "multivariate spread" among groups, which is similar to variance homogeneity in univariate ANOVA. To test if one or more groups is more variable than the others, a PERMDISP can be conducted in R. 
 
 # Create distance matrix
-d_df <- vegdist(df, method="bray") 
+d_df <- vegdist(df, method="euclidean") 
 
 bd <- betadisper(d_df,meta$cdate, type = "centroid")
 boxplot(bd)
